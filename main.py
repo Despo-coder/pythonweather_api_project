e7dce8c11c6937f7a14df5eb6b469eaf0228617c
Despo_the_Coder <damion.wilson@gmail.com>
2023-09-21 13:52:08 -0400

    Updated final route to get data by year and station

--- main.py
+++ main.py
@@ -17,6 +17,21 @@ def about(station, date):
     temp=df.loc[df['    DATE']==date]['   TG'].squeeze()/10
     return {'Station': station, 'Date': date, 'Temperature': temp}
 
+@app.route('/api/v1/<station>/')
+def data_by_stationId(station):
+    filename = './data/TG_STAID' + str(station).zfill(6) +'.txt'
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    temp=df.to_dict(orient='records')
+    return {'Station': station, 'Temperature': temp}
+
+@app.route('/api/v1/yearly/<station>/<year>')
+def data_by_year(station, year):
+    filename = './data/TG_STAID' + str(station).zfill(6) +'.txt'
+    df = pd.read_csv(filename, skiprows=20)
+    df['    DATE']= df['    DATE'].astype(str)
+    data = df[df['    DATE'].str.startswith(str(year))].to_dict(orient='records')
+    return data
+
 
 if __name__ == '__main__':
     app.run(debug=True)
